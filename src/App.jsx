d8c0aaf70e9aef99eb771932acabc7c11fc740f6
GoldDeniel <aranyadhun@gmail.com>
2024-11-03 11:08:32 +0100

    :rocket: fixed edit

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ function App() {
   const uri = 'http://localhost:5289/api/TodoItems/';
   const [todos, setTodos] = useState([]);
   const [newTodoName, setNewTodoName] = useState('');
-
+  const [editTodoId, setEditTodoId] = useState(null);
+  const [editTodoName, setEditTodoName] = useState('');
+  
   async function displayItems(uri) {
     try {
       const items = await getItems(uri);
-      setTodos(items); // Update the state with fetched items
+      setTodos(items);
     } catch (error) {
       console.error('Error fetching items:', error);
     }
@@ -69,6 +71,42 @@ function App() {
       console.error('Error:', error);
     }
   }
+  const handleLongPress = (todo) => {
+    setEditTodoId(todo.Id);
+    setEditTodoName(todo.Name);
+  };
+
+  const handleEditChange = (e) => {
+    setEditTodoName(e.target.value);
+  };
+
+  const handleFocusOff = () => {
+    // updateItem(uri, todo.Id, { Id: todo.Id, Name: editTodoName, IsComplete: todo.IsComplete });
+    setEditTodoId(null);
+  };
+
+  let timer;
+  const startPressTimer = (todo) => {
+    timer = setTimeout(() => handleLongPress(todo), 500); // fel masodperc, nem kell 500 masodpercig nyomni :)
+  };
+
+  const cancelPressTimer = () => {
+    clearTimeout(timer);
+  };
+
+  const handleNewTodoKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      addItem(uri, { Name: newTodoName, IsComplete: false });
+      setNewTodoName('');
+    }
+  };
+
+  const handleModifyKeyDown = (todo) => (e) => {
+    if (e.key === 'Enter') {
+      handleFocusOff(todo);
+      updateItem(uri, todo.Id, { Id: todo.Id, Name: editTodoName, IsComplete: todo.IsComplete });
+    }
+  };
 
   useEffect(() => {
     displayItems(uri);
@@ -93,7 +131,28 @@ function App() {
                     updateItem(uri, todo.Id, {Id: todo.Id, Name: todo.Name, IsComplete: !todo.IsComplete });
                   }} 
                 />
-                <label htmlFor={`checkbox-${todo.Id}`}>{todo.Name}</label>
+                {editTodoId === todo.Id ? (
+                  <input 
+                    type="text" 
+                    className='edit-input'
+                    value={editTodoName} 
+                    onChange={handleEditChange} 
+                    onBlur={() => handleFocusOff()}
+                    onKeyDown={handleModifyKeyDown(todo)}
+                    autoFocus
+                  />
+                ) : (
+                  <label 
+                    htmlFor={`checkbox-${todo.Id}`} 
+                    onMouseDown={() => startPressTimer(todo)}
+                    onMouseUp={cancelPressTimer}
+                    onMouseLeave={cancelPressTimer}
+                    onTouchStart={() => startPressTimer(todo)}
+                    onTouchEnd={cancelPressTimer}
+                  >
+                    {todo.Name}
+                  </label>
+                )}
               </div>
               <button onClick={() => deleteItem(uri, todo.Id)}>Delete</button>
             </li>
@@ -102,7 +161,13 @@ function App() {
         
         
 
-        <input type="text" value={newTodoName} onChange={(e) => setNewTodoName(e.target.value)} />
+        <input 
+        type="text" 
+        value={newTodoName} 
+        onChange={(e) => setNewTodoName(e.target.value)}
+        onKeyDown={handleNewTodoKeyDown}
+
+        />
         <button onClick={() => {
 
           addItem(uri, { Name: newTodoName, IsComplete: false })
