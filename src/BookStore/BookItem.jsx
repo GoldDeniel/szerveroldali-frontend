c1e974eae279c57523619a689805b0312b4eef15
GoldDeniel <aranyadhun@gmail.com>
2024-11-22 20:54:55 +0100

    dolar

--- src/BookStore/BookItem.jsx
+++ src/BookStore/BookItem.jsx
@@ -9,7 +9,7 @@ function BookItem({
       <h2 className="book-item-spacing">{book.Name}</h2>
       <p className="book-item-spacing">{book.Category}</p>
       <p className="book-item-spacing">{book.Author}</p>
-      <p className="book-item-spacing">{book.Price}</p>
+      <p className="book-item-spacing">{book.Price} $</p>
       {!isModifying ? (
         <button
           className="book-item-spacing-button"
